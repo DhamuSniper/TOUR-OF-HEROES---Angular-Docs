9a71e8d9b7d3e47df706df8cc9b1b7251849dee7
dhamusniper <dhamusniper@yahoo.com>
2019-03-29 12:36:40 +0530

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero } from './hero';
+
+export const HEROES:Hero[]=[
+    { id:1,name:'Iron Man'},
+    { id:2,name:'Captain America'},
+    { id:3,name:'Thor'},
+    { id:4,name:'Hulk'},
+    { id:5,name:'Black widow'},
+    { id:6,name:'Hawk Eye'},
+    { id:7,name:'Captain Marvel'},
+    { id:8,name:'Spider Man'},
+    { id:9,name: 'Groot'},
+    { id:10,name:'Black Panther'}
+]
